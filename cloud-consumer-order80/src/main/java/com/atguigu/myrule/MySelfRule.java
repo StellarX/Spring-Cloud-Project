479c11fed57a114c225533060db04351d090d1fe
qqyang <2605578650@qq.com>
2020-11-12 21:17:31 +0800

    OrderMain80再次运行失败

--- cloud-consumer-order80/src/main/java/com/atguigu/myrule/MySelfRule.java
+++ cloud-consumer-order80/src/main/java/com/atguigu/myrule/MySelfRule.java
@@ -1,7 +1,7 @@
 package com.atguigu.myrule;
 
-import com.netflix.loadbalancer.IRule;
-import com.netflix.loadbalancer.RandomRule;
+//import com.netflix.loadbalancer.IRule;
+//import com.netflix.loadbalancer.RandomRule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,9 +12,9 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class MySelfRule
 {
-    @Bean
-    public IRule myRule()
-    {
-        return new RandomRule();//定义为随机
-    }
+//    @Bean
+//    public IRule myRule()
+//    {
+//        return new RandomRule();//定义为随机
+//    }
 }
--- cloud-consumer-order80/src/main/java/com/atguigu/springcloud/OrderMain80.java
+++ cloud-consumer-order80/src/main/java/com/atguigu/springcloud/OrderMain80.java
@@ -4,8 +4,8 @@ import com.atguigu.myrule.MySelfRule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 //import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.ribbon.RibbonClient;
-import org.springframework.cloud.netflix.ribbon.RibbonClients;
+//import org.springframework.cloud.netflix.ribbon.RibbonClient;
+//import org.springframework.cloud.netflix.ribbon.RibbonClients;
 
 /**
  * @auther zzyy
@@ -17,6 +17,6 @@ import org.springframework.cloud.netflix.ribbon.RibbonClients;
 public class OrderMain80
 {
     public static void main(String[] args) {
-            SpringApplication.run(OrderMain80.class, args);
+        SpringApplication.run(OrderMain80.class, args);
     }
 }
