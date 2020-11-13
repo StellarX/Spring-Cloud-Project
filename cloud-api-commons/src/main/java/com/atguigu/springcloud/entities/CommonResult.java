3c331226f4aa114453a503c02097ab3fbfb8d2d5
qqyang <2605578650@qq.com>
2020-11-13 14:30:55 +0800

    80服务成功运行（注意改数据库密码）

--- cloud-api-commons/src/main/java/com/atguigu/springcloud/entities/CommonResult.java
+++ cloud-api-commons/src/main/java/com/atguigu/springcloud/entities/CommonResult.java
@@ -21,4 +21,9 @@ public class CommonResult<T>
     {
         this(code,message,null);
     }
+//    public CommonResult(Integer code,String message, T data){
+//        this.code = code;
+//        this.message = message;
+//        this.data = data;
+//    }
 }
--- cloud-consumer-order80/src/main/java/com/atguigu/springcloud/controller/OrderController.java
+++ cloud-consumer-order80/src/main/java/com/atguigu/springcloud/controller/OrderController.java
@@ -7,9 +7,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
 import javax.annotation.Resource;
@@ -24,9 +22,9 @@ import java.util.List;
 @Slf4j
 public class OrderController
 {
-    //public static final String PAYMENT_URL = "http://localhost:8001";
+    public static final String PAYMENT_URL = "http://localhost:8001";
 
-    public static final String PAYMENT_URL = "http://CLOUD-PAYMENT-SERVICE";
+//    public static final String PAYMENT_URL = "http://CLOUD-PAYMENT-SERVICE";
 
     @Resource
     private RestTemplate restTemplate;
@@ -36,8 +34,8 @@ public class OrderController
     @Resource
     private DiscoveryClient discoveryClient;
 
-    @GetMapping("/consumer/payment/create")
-    public CommonResult<Payment> create(Payment payment)
+    @PostMapping("/consumer/payment/create")
+    public CommonResult<Payment> create(@RequestBody Payment payment)
     {
         return restTemplate.postForObject(PAYMENT_URL +"/payment/create",payment,CommonResult.class);
     }
