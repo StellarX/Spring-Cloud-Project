bce2c431e688657d69ff96f370848a87215c437f
qqyang <2605578650@qq.com>
2020-11-15 23:04:34 +0800

    actuator；EnableDiscoveryClient

--- cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/PaymentMain8001.java
+++ cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/PaymentMain8001.java
@@ -11,7 +11,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
  */
 @SpringBootApplication
 @EnableEurekaClient
-//@EnableDiscoveryClient
+@EnableDiscoveryClient
 public class PaymentMain8001
 {
     public static void main(String[] args) {
