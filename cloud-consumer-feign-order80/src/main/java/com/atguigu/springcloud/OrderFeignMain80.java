88f3d46d2b0ac83ed7f4774c2dc27e1b32662abf
qqyang <2605578650@qq.com>
2021-11-20 17:45:55 +0800

    open feign 设置负载均衡 以及 超时配置

--- cloud-consumer-feign-order80/src/main/java/com/atguigu/springcloud/OrderFeignMain80.java
+++ cloud-consumer-feign-order80/src/main/java/com/atguigu/springcloud/OrderFeignMain80.java
@@ -1,7 +1,9 @@
 package com.atguigu.springcloud;
 
+import com.atguigu.myrule.MySelfRule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
@@ -10,6 +12,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
  */
 @SpringBootApplication
 @EnableFeignClients
+@RibbonClient(name = "CLOUD-PAYMENT-SERVICE",configuration= MySelfRule.class)
 public class OrderFeignMain80
 {
     public static void main(String[] args) {
