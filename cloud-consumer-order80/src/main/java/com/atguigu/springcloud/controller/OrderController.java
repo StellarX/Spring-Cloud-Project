34d3eb44d20f09e2ac253fdafb784eb08f6a97ec
qqyang <2605578650@qq.com>
2021-11-20 15:43:30 +0800

    ribbon 随机负载均衡

--- cloud-consumer-order80/src/main/java/com/atguigu/springcloud/controller/OrderController.java
+++ cloud-consumer-order80/src/main/java/com/atguigu/springcloud/controller/OrderController.java
@@ -24,8 +24,7 @@ public class OrderController
 {
 //    public static final String PAYMENT_URL = "http://localhost:8001";
 
-//    public static final String PAYMENT_URL = "http://CLOUD-PAYMENT-SERVICE";
-    public static final String PAYMENT_URL = "http://cloud-payment-service";
+    public static final String PAYMENT_URL = "http://CLOUD-PAYMENT-SERVICE";
 
     @Resource
     private RestTemplate restTemplate;
