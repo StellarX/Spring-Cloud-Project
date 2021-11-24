9f57e7764a81c7f56eaa9f045e7e3f66b21822db
qqyang <2605578650@qq.com>
2021-11-24 22:48:25 +0800

    Hystrix服务熔断

--- cloud-consumer-hystrix-dashboard9001/src/main/java/com/atguigu/springcloud/HystrixDashboardMain9001.java
+++ cloud-consumer-hystrix-dashboard9001/src/main/java/com/atguigu/springcloud/HystrixDashboardMain9001.java
@@ -9,7 +9,7 @@ import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboar
  * @create 2020-02-20 22:02
  */
 @SpringBootApplication
-@EnableHystrixDashboard
+@EnableHystrixDashboard//服务启动后访问http://localhost:9001/hystrix
 public class HystrixDashboardMain9001
 {
     public static void main(String[] args) {
